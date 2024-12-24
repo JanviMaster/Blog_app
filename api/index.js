0fa4edad5443955e058ba96ab08ebc4dd9ddb35f
Janvi Master <janvimaster@gmail.com>
2024-12-24 23:20:53 +0530

    10th update test api routes

--- api/index.js
+++ api/index.js
@@ -1,18 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.routes.js';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=> {console.log("mongodb is connected");
-})
-.catch(
-    ()=> {console.log(err);
- });
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+  });
 
 const app = express();
 
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
 
 app.listen(3000, () => { 
-console.log('Server is running on port 3000!');
+  console.log('Server is running on port 3000!');
 });
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
       <Route path="/explore" element={<Explore/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/sign-in" element={<Signin/>}/>
-      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/register" element={<SignUp/>}/>
       <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/projects" element={<Projects/>}/>
       </Routes>
