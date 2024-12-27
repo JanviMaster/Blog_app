d4c2518bafbcf2b9b915ffa6b17484a3aa764f28
Janvi Master <janvimaster@gmail.com>
2024-12-27 23:03:09 +0530

    Added a middleware and errorHandler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,33 +1,31 @@
 import User from "../models/user.models.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
-    const { username, email, password } = req.body;
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-    if (
-        !username ||
-        !email ||
-        !password ||
-        username === '' ||
-        email === '' ||
-        password === ''
-      ) {
-        next(errorHandler(400, 'All fields are required'));
-      }
+ 
+  if (!username || !email || !password) {
+    return next(errorHandler(400, "All fields are required"));
+  }
 
-      const hashedPassword= bcryptjs.hashSync(password,10);
+  try {
+    const hashedPassword = await bcryptjs.hash(password, 10);
 
-      const newUser = new User({
-        username,
-        email,
-        password: hashedPassword,
-      });
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "Signup successful" });
+  } catch (error) {
     
-      try {
-        await newUser.save();
-        res.json('Signup successful');
-      } catch (error) {
-        next(error);
-        res.status(500).json({ message: error.message });
-      }
-    };
+    if (error.code === 11000) {
+      return next(errorHandler(400, "Email already in use"));
+    }
+    next(error);
+  }
+};
