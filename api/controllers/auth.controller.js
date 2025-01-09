cbed691628fd377b22000d13ef9ffaef0526bab3
Janvi Master <janvimaster@gmail.com>
2025-01-09 22:20:53 +0530

    signin api routes created 17th update

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,7 @@
 import User from "../models/user.models.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-import jwt from JsonWebTokenError;
-import { json } from "express";
-import { JsonWebTokenError } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -27,28 +25,32 @@ export const signup = async (req, res, next) => {
   }
 };
 
-export const signin = async (req,res,next) => {
-  const {email, password} = req.body;
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
 
-  if(!email || !password || username=== ''|| password=== ''){
-    next(errorHandler(400, 'All fields are required'));
+  if (!email || !password) {
+    return next(errorHandler(400, "All fields are required"));
   }
-  try{
-    const validUser = await User.findOne({email});
-    if(!validUser){
-      next(errorHandler(404,'User not found'));
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
     }
-    const validPassword= bcryptjs.compareSync(password,validUser.password);
-    if(!validPassword){
-      next(errorHandler(400,'Invalid password'));
+    const validPassword = await bcryptjs.compare(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
     }
-    // Continue from here 
-    // const token= jwt.sign(
-    //   {
-    
-    //   }
-    // )
-  }catch(error){
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const {password: pass ,...rest} = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
     next(error);
   }
-}
+};
